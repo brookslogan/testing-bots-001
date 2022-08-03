a497228884da61ea6e005286613a511a07261499
Logan C. Brooks <lcbrooks@andrew.cmu.edu>
2022-08-03 13:10:15 -0700

    Use --fail as ubuntu-latest doesn't have --fail-with-body

--- .github/scripts/notify-about-stale-questions.sh
+++ .github/scripts/notify-about-stale-questions.sh
@@ -9,7 +9,11 @@ echo "if there is a problem, we expect to forward a non-0 exit status from curl,
 echo "but not to output anything further."
 
 # not sure what all curl can output; use --silent and &> /dev/null to not overly
-# rely on GitHub's scrubbing. Use --fail-with-body to actually get a sensible
-# exit status on errors, and --location to follow redirects + get a sensible
-# output status on redirects, including when default max-redirs (50) is hit.
-curl --fail-with-body --location --request POST "$SECRET_STALE_QUESTION_NOTIFY_WEBHOOK"
+# rely on GitHub's scrubbing. Use --fail to try to get a sensible exit status on
+# errors, and --location to follow redirects + get a sensible output status on
+# redirects, including when default max-redirs (50) is hit.
+#
+# FIXME --fail can have some things slip through, according to curl docs.
+# --fail-with-body doesn't appear to have the same issues, but is new and isn't
+# recognized on ubuntu-latest at time of writing.
+curl --fail --location --request POST "$SECRET_STALE_QUESTION_NOTIFY_WEBHOOK"
