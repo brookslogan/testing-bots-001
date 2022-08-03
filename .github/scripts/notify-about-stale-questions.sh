617426596fb384a9f11aa827c0da709ab2045676
Logan C. Brooks <lcbrooks@andrew.cmu.edu>
2022-08-03 13:05:32 -0700

    Try to make curl output a sensible exit status

--- .github/scripts/notify-about-stale-questions.sh
+++ .github/scripts/notify-about-stale-questions.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-# exit on any error, or reference to an undefined variable
+# Exit on any error, or reference to an undefined variable; at time of writing,
+# these are defensive rather than relied upon.
 set -eu
 
-# not sure what all curl can output; use --silent and &> /dev/null to not overly rely on GitHub's scrubbing
-curl --silent --request POST "$SECRET_STALE_QUESTION_NOTIFY_WEBHOOK" &> /dev/null
+echo "Attempting to trigger webhook by using curl without output;"
+echo "if there is a problem, we expect to forward a non-0 exit status from curl,"
+echo "but not to output anything further."
+
+# not sure what all curl can output; use --silent and &> /dev/null to not overly
+# rely on GitHub's scrubbing. Use --fail-with-body to actually get a sensible
+# exit status on errors, and --location to follow redirects + get a sensible
+# output status on redirects, including when default max-redirs (50) is hit.
+curl --fail-with-body --location --request POST "$SECRET_STALE_QUESTION_NOTIFY_WEBHOOK"
